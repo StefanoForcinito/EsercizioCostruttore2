6619b0776f43f8921e457a6006ee0b4d855db411
Stefano <stegallo1990@gmail.comgit config -lgit config --global user.email stegallo1990@gmail.com>
2023-11-29 19:39:00 +0100

    aggiunto classe auto

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        Auto auto = new Auto(1000,"AA243FF","Volvo","Serie 1");
+
+
+        System.out.println("Auto: " + auto);
+
+    }
 }
